12d2665f90b6c293d1974162a28a9b08ad544fce
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-03-09 05:57:28 +0400

    add database setup

---
+++ Backend/CloudStorage.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CloudStorage.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using File = CloudStorage.Domain.Entities.File;
+
+namespace CloudStorage.Persistence;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) => Database.Migrate();
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Folder> Folders { get; set; }
+    public DbSet<File> Files { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // builder.ApplyConfiguration();
+        base.OnModelCreating(builder);
+    }
+}
---
+++ Backend/CloudStorage.Persistence/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using CloudStorage.Domain.Interfaces;
+using CloudStorage.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Minio;
+
+namespace CloudStorage.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgreSql");
+
+        services.AddMinio(options => options
+            .WithEndpoint(configuration["MinIO:Endpoint"])
+            .WithCredentials(configuration["MinIO:AccessKey"], configuration["MinIO:SecretKey"])
+            .WithSSL(false)
+            .Build());
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        services.AddScoped<IFileRepository, FileRepository>();
+        services.AddScoped<IFolderRepository, FolderRepository>();
+        services.AddScoped<IStorageRepository, StorageRepository>();
+
+        return services;
+    }
+}
