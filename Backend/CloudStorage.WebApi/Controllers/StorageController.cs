08ad5c4f306323e13c7f49f64915042fdeec7852
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-03-09 06:05:02 +0400

    add storage controller

---
+++ Backend/CloudStorage.WebApi/Controllers/StorageController.cs
@@ -0,0 +1,50 @@
+﻿using CloudStorage.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorage.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StorageController : ControllerBase
+{
+    private readonly IStorageService _storageService;
+
+    public StorageController(IStorageService storageService)
+    {
+        _storageService = storageService;
+    }
+
+    [HttpGet("getFile")]
+    public async Task DownloadFile(Guid fileId)
+    {
+        var userId = 0; // пока аунтификации нет
+        var fileDetails = await _storageService.GetFileDetailsAsync(fileId, userId);
+
+        Response.ContentLength = fileDetails.Length;
+        Response.ContentType = fileDetails.MimeType;
+        Response.Headers.Add("Content-Disposition", $"attachment; filename={fileDetails.Name}");
+
+        async Task StreamFileAction(Stream stream, CancellationToken ct)
+        {
+            await stream.CopyToAsync(Response.BodyWriter.AsStream(), ct);
+        }
+
+        await _storageService.GetFileAsync(fileId, StreamFileAction);
+    }
+
+    [HttpPost("uploadFiles")]
+    public async Task<IActionResult> UploadFiles(IFormFileCollection files, Guid folderId)
+    {
+        await _storageService.PutFilesAsync(files, folderId);
+
+        return Ok();
+    }
+
+    [HttpPost("uploadFolder")]
+    public async Task<IActionResult> UploadFolder(IFormFile zipFolder, Guid parentFolderId)
+    {
+        await _storageService.PutFolderAsync(zipFolder, parentFolderId);
+
+        return Ok();
+    }
+}
